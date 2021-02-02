7dd81fc46a195e9f1ee2bd19f34fdea3b8077a73
Kevin Tran <tran94@sheridancollege.ca>
2021-02-01 21:22:32 -0500

    Initial commit

---
+++ src/main/java/ca/kevin/assignment1_kt/Assignment1KtApplication.java
@@ -0,0 +1,13 @@
+package ca.kevin.assignment1_kt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1KtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment1KtApplication.class, args);
+    }
+
+}
