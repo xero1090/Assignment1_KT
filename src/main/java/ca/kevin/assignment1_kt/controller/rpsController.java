edfdf81a204189ddc5d83c018b95d260872cf5ba
Kevin Tran <tran94@sheridancollege.ca>
2021-02-01 23:04:53 -0500

    Renamed static to static.css, finished Envelope, and output (for now)

--- src/main/java/ca/kevin/assignment1_kt/controller/rpsController.java
+++ src/main/java/ca/kevin/assignment1_kt/controller/rpsController.java
@@ -1,4 +1,28 @@
 package ca.kevin.assignment1_kt.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+import ca.kevin.assignment1_kt.domain.Envelope;
+
+@Controller
 public class rpsController {
+    private final Logger logger = LoggerFactory.getLogger(rpsController.class);
+
+    @GetMapping(value = {"/", "/Input"})
+    public String input(){
+    logger.trace("Input is called");
+    return "Input";
+    }
+
+    @GetMapping("/Process")
+    public ModelAndView process(@ModelAttribute Envelope envelope){
+    logger.trace("Process is called");
+    logger.debug("envelope = " + envelope);
+    return new ModelAndView("Output", "envelope", envelope);
+    }
+
 }
--- src/main/java/ca/kevin/assignment1_kt/domain/Envelope.java
+++ src/main/java/ca/kevin/assignment1_kt/domain/Envelope.java
@@ -1,4 +1,24 @@
 package ca.kevin.assignment1_kt.domain;
 
-public class Envelope {
+import java.io.Serializable;
+
+public class Envelope implements Serializable {
+
+    private int message = 1;
+    public Envelope(){
+
+    }
+
+    public int getMessage() {
+        return message;
+    }
+
+    public void setMessage(int message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString(){
+    return "Envelope{" + "message=" + message +'}';
+    }
 }
